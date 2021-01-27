19aaab7e72458191dc051020751a2ee487d96116
Graham Onak <graham.onak@gmail.com>
2021-01-27 06:00:36 -0600

    asd

--- js/made.js
+++ js/made.js
@@ -381,14 +381,13 @@
 			$('body').addClass('menu--open');
 		}
 	});
-	$(document).on('click', '.menu__list__item__link', function () {
-    $(document).getElementsByClassName('.js-menu-toggle').click();
+	$(document).on('click', '#menu_item', function () {
 		// If menu is open when you click a link on mobile
 	//	if ($('.menu').hasClass('menu--open')) {
 //			$('.menu').removeClass('menu--open');
-  //      if ($('body').hasClass('menu--open')) {
-	//		$('body').removeClass('menu--open');
-//		}
+        if ($('body').hasClass('menu--open')) {
+			$('body').removeClass('menu--open');
+		}
 	});
 
 
